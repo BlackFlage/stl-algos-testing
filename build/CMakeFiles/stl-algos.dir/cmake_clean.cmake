501dec6053ac27113e668135673b417d8c33689e
BlackFlage <BlackFlage10@gmail.com>
2021-12-27 23:33:15 +0000

    First commit

---
+++ build/CMakeFiles/stl-algos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stl-algos.dir/sorting.cpp.o"
+  "stl-algos"
+  "stl-algos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stl-algos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
